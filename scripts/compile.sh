8ff8b59355b6be8c5793623dd402e9f9e2d63868
KasunEdward <kasunedward48@gmail.com>
2021-11-20 00:08:26 +0530

    Add changes

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,6 @@
 mkdir -p /opt/sample_app
-chown owner /opt/sample_app
-export PATH=/home/kasun/.cache/rebar3/bin:$PATH
+chown ubuntu /opt/sample_app
+export PATH=$PATH:~/.cache/rebar3/bin
 rebar3 as prod tar
 cp _build/prod/rel/sample_app/sample_app-1.0.tar.gz /opt/sample_app
 cd /opt/sample_app
